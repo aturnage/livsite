5d865a7342c0a3392ddaee96bd294ec4e0816468
Andrew <aturnage@hotmail.com>
2019-01-07 14:44:03 -0600

    Added links to print page and created print grid

--- scripts/printImages.js
+++ scripts/printImages.js
@@ -1,12 +1,38 @@
 var printImages = [
   {
-    images:['abdul-pic.png', 'AlbumCover(Final).png', 'ArticleLayout2.png', 'BlackAlumniCHS98.jpg', 'BlackMardiGrasFlyer-Glow.jpg', 'bpcs-logo.png', 'BrandyCruesoeLogo(combine).png']
-  }
+    id: 0,
+    images:['abdul-pic.png']
+},
+{
+  id: 1,
+  images:['AlbumCover(Final).png']
+},
+{
+  id: 2,
+  images:['ArticleLayout2.png']
+},
+{
+  id: 3,
+  images:['BlackAlumniCHS98.jpg']
+},
+{
+  id: 4,
+  images:['BlackMardiGrasFlyer-Glow.jpg']
+},
+{
+  id: 5,
+  images:['bpcs-logo.png']
+},
+{
+  id: 6,
+  images:['BrandyCruesoeLogo(combine).png']
+}
 ]
 // function shine(){
   var printer = document.querySelector(".print")
   printImages.forEach(function (pics){
     printer.innerHTML += `<div class="clientFormat">
     <div class='image'>${pics.images.map(img => `<img src="/printImages/${img}">`)}</div>
-    </div>`
+    </div>
+    `
   })
